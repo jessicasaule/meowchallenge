63d601b7285ddd0e741ef47469f1e20b99bc3a01
Lucas Acosta <lucas@meowtel.com>
2021-07-16 12:18:15 -0700

    Let's start

---
+++ meow-back/src/routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const cats = require('../db')
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+    try {
+        let {color} = req.query;
+
+        //Validates whether the cat color is valid or not
+        if(color = 'grey' || 'white' || 'brown'){
+            let catJson = JSON.parse(cats)
+
+            let selectedCat = catJson[color]
+            
+            return res.send({data: selectedCat}).status(200)
+        }
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
+
+module.exports = router;
