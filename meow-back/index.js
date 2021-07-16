99b9efa1205d992492d89f48e43bea0ff7bb548e
Lucas Acosta <lucas@meowtel.com>
2021-07-16 12:17:39 -0700

    Let's start

--- meow-back/index.js
+++
@@ -1,5 +0,0 @@
-const server = require('./src/app.js');
-
-server.listen(3001, () => {
-console.log('Mini api listening at 3001'); // eslint-disable-line no-console
-});
--- meow-back/src/db/index.js
+++
@@ -1,7 +0,0 @@
-let cats = `{
-    "white": {"img": "https://www.catbreedslist.com/uploads/cat-pictures/persian-cat-2.jpg"},
-    "brown": {"img": "https://www.catbreedslist.com/uploads/cat-pictures/havana-brown-2.jpg"} ,  
-    "grey": {"img": "https://www.catbreedslist.com/uploads/cat-pictures/british-shorthair-2.jpg"}
-}`;
-
-module.exports = cats;
